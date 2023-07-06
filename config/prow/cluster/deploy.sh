5b740744aa21edc027cfe629af4802397e5bd081
Max Horstmann <maxhorstmann@microsoft.com>
2023-07-06 17:03:08 -0400

    clean up deploy script, now that it runs in workflow

--- config/prow/cluster/deploy.sh
+++
@@ -1,7 +0,0 @@
-export LOCATION=westus2
-export RESOURCE_GROUP_NAME=aks-lts-prow
-az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
-az deployment group create \
-  --resource-group $RESOURCE_GROUP_NAME \
-  --template-file prow-cluster.bicep \
-  --parameters aks_cluster_region=$LOCATION
